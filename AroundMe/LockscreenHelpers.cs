146c5071a029ea5bc772aacdcfd3f0512a871510
DYDYD <njlxyaoxinwei@gmail.com>
2013-10-24 16:22:09 -0400

    Finished Tutorial Part32&33, added changing lockscreen and tile cycle

---
+++ AroundMe/LockscreenHelpers.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Phone.Shell;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Phone.System.UserProfile;
+
+namespace AroundMe
+{
+    class LockscreenHelpers
+    {
+        private const string BackgroundRoot = "Images/";
+        private const string IconRoot = "Shared/ShellContent/";
+        private const string LockScreenData = "LockScreenData.json";
+        public static void CleanStorage()
+        {
+            using (IsolatedStorageFile storageFolder = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                TryToDeleteAllFiles(storageFolder, BackgroundRoot);
+                TryToDeleteAllFiles(storageFolder, IconRoot);
+            }
+        }
+        public static void TryToDeleteAllFiles(IsolatedStorageFile storageFolder, string directory)
+        {
+            if (storageFolder.DirectoryExists(directory))
+            {
+                try
+                {
+                    string[] files = storageFolder.GetFileNames(directory);
+                    foreach (string File in files)
+                    {
+                        storageFolder.DeleteFile(directory + files);
+
+                    }
+                }
+                catch (Exception)
+                {
+                    //ignoring the exception
+                }
+             }
+        }
+        public static void SaveSelectedBackgroundScreens(List<FlickrImage> data)
+        {
+            string stringData = JsonConvert.SerializeObject(data);
+            using (var storageFolder = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (var stream = storageFolder.CreateFile(LockScreenData))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(stringData);
+                    }
+                } 
+            }
+            
+        }
+        public static void SetRandomImageFromLocalStorage()
+        {
+            string fileData;
+            using (IsolatedStorageFile storageFolder = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (!storageFolder.FileExists(LockScreenData))
+                    return;
+                using (IsolatedStorageFileStream stream = storageFolder.OpenFile(LockScreenData, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        fileData = reader.ReadToEnd();
+                    }
+                }
+
+            
+            }
+            List<FlickrImage> images = JsonConvert.DeserializeObject<List<FlickrImage>>(fileData);
+            if (images != null)
+            {
+                Random rndNumber = new Random();
+                int index = rndNumber.Next(images.Count);
+                //set img as lockscreen
+                SetImage(images[index].Image1024);
+            } 
+        }
+        public static async void SetImage(Uri uri)
+        {
+            string fileName = uri.Segments[uri.Segments.Length - 1];
+            string imageName = BackgroundRoot + fileName;
+            string iconName = IconRoot + fileName;
+            using (IsolatedStorageFile storageFolder = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (!storageFolder.DirectoryExists(BackgroundRoot))
+                    storageFolder.CreateDirectory(BackgroundRoot);
+
+                if (!storageFolder.FileExists(imageName))
+                {
+
+
+                    using (IsolatedStorageFileStream stream = storageFolder.CreateFile(imageName))
+                    {
+                        HttpClient client = new HttpClient();
+                        byte[] flickrResult = await client.GetByteArrayAsync(uri);
+                        await stream.WriteAsync(flickrResult, 0, flickrResult.Length);
+                    }
+                    storageFolder.CopyFile(imageName, iconName);
+                }
+
+            }
+            //set the lockscreen
+            SetLockscreen(fileName);
+        }
+        private static async void SetLockscreen(string fileName)
+        {
+            bool hasAccessForLockscreen = LockScreenManager.IsProvidedByCurrentApplication;
+            if (!hasAccessForLockscreen)
+            {
+                var accessRequested = await LockScreenManager.RequestAccessAsync();
+                hasAccessForLockscreen = (accessRequested == LockScreenRequestResult.Granted);
+            }
+
+            if (hasAccessForLockscreen)
+            {
+                Uri imgUri = new Uri("ms-appdata:///Local/" + BackgroundRoot + fileName, UriKind.Absolute);
+                LockScreen.SetImageUri(imgUri);
+            }
+
+            var mainTile = ShellTile.ActiveTiles.FirstOrDefault();
+
+            if (mainTile != null)
+            {
+                Uri iconUri = new Uri("isostore:/" + IconRoot + fileName, UriKind.Absolute);
+                var imgs = new List<Uri>();
+                imgs.Add(iconUri);
+                CycleTileData tileData = new CycleTileData();
+                tileData.CycleImages = imgs;
+                mainTile.Update(tileData);
+            }
+        }
+
+    }
+}
--- AroundMe/SearchResults.xaml.cs
+++ AroundMe/SearchResults.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Media.Animation;
+using System.Diagnostics;
 
 namespace AroundMe
 {
@@ -21,15 +23,62 @@ namespace AroundMe
         {
             InitializeComponent();
             Loaded += SearchResults_Loaded;
+            BuildLocalizedApplicationBar();
         }
 
         async void SearchResults_Loaded(object sender, RoutedEventArgs e)
         {
-            // LocationTextBlock.Text = string.Format("Location: {0} & {1}", _latitude, _longitude);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayProgressBar.IsIndeterminate = true;
+
             var images = await FlickrImage.GetFlickrImages(flickrApiKey, _topic, _latitude, _longitude, _radius);
             DataContext = images;
 
+            if (images.Count == 0)
+                NoPhotosFound.Visibility = Visibility.Visible;
+            else
+                NoPhotosFound.Visibility = Visibility.Collapsed;
+            Overlay.Visibility = Visibility.Collapsed;
+            OverlayProgressBar.IsIndeterminate = false;
+
         }
+
+        // Sample code for building a localized ApplicationBar
+        private void BuildLocalizedApplicationBar()
+        {
+
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
+            ApplicationBar.IsVisible = false;
+            // Create a new button and set the text value to the localized string from AppResources.
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Toolkit.Content/ApplicationBar.Check.png", UriKind.Relative));
+            appBarButton.Text = "set";
+            appBarButton.Click += appBarButton_Click;
+            ApplicationBar.Buttons.Add(appBarButton);
+        }
+
+        private void appBarButton_Click(object sender, EventArgs e)
+        {
+            List<FlickrImage> imgs = new List<FlickrImage>();
+
+            foreach (object item in PhotosForLockscreen.SelectedItems)
+            {
+                FlickrImage img = item as FlickrImage;
+                if (img != null)
+                    imgs.Add(img);
+
+            }
+            //remove all images in IsolatedStorage
+            LockscreenHelpers.CleanStorage();
+            //Save new list
+            LockscreenHelpers.SaveSelectedBackgroundScreens(imgs);
+            //Randomly select one item as lockscreen
+            LockscreenHelpers.SetRandomImageFromLocalStorage();
+
+            MessageBox.Show("You have a new background!", "Set!",MessageBoxButton.OK);
+
+        }
+
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -45,6 +94,25 @@ namespace AroundMe
 
         private void PhotosForLockscreen_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (PhotosForLockscreen.SelectedItems.Count == 0)
+                ApplicationBar.IsVisible = false;
+            else
+                ApplicationBar.IsVisible = true;
+        }
+
+        private void Image_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            Image img = sender as Image;
+            if (img == null)
+                return;
+            Storyboard s = new Storyboard();
+            DoubleAnimation doubleAni = new DoubleAnimation();
+            doubleAni.To = 1;
+            doubleAni.Duration = new Duration(TimeSpan.FromMilliseconds(500));
+            Storyboard.SetTarget(doubleAni, img);
+            Storyboard.SetTargetProperty(doubleAni, new PropertyPath(OpacityProperty));
+            s.Children.Add(doubleAni);
+            s.Begin(); //Have to code this in C# because we cannot target elements in the List in XAML.
 
         }
 
